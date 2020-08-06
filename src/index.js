85ed2ddeade61414bf2acd9856135b0f6c440e7f
DESKTOP-FM8TTEO\Administrator <cici2606634321@gmail.com>
2020-08-06 14:02:44 +0800

    01

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import React from 'react'
+import ReactDom from 'react-dom'
+// const app=<h1>22222</h1>
+const App=(props)=>{
+  
+return <h1>12313123{props.title}</h1>
+}
+// const app=creatApp(
+//     {
+//         title:'wwerwere'
+//     }
+// )
+ReactDom.render(
+    <App title="1111"></App>,
+    document.querySelector('#root')
+)
